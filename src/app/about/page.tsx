b75f0db4f09798a1f2f344f22446ab167f3abeb8
fbhatti97 <141708863+fbhatti97@users.noreply.github.com>
2025-03-20 09:01:03 +1100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <main>
       <h1>About Page</h1>
-      <p>This is a static page that doesn't require any data fetching.</p>
+      <p>This is a static page that doesn&apos;t require any data fetching.</p>
       <p>
         <Link href="/">Back to Home</Link>
       </p>
